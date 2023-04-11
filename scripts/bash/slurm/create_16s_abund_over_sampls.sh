d9b9e12cffa99db8a8e8b904a99ffd84c3a3bec9
Your Name <you@example.com>
2023-04-11 10:48:28 +0200

    Added two scripts

---
+++ scripts/bash/slurm/create_16s_abund_over_sampls.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -J LB_hr 
+#SBATCH --qos=normal
+#SBATCH --time=4:30:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+#SBATCH -e /zfshome/sukem127/error_hr_%A_%a.log
+#SBATCH -o /zfshome/sukem127/output_hr_%A_%a.log
+
+args=("$@")  # Access all arguments as an array
+echo "Current Task ID is ${SLURM_ARRAY_TASK_ID}"
+echo "Currently working on ${args[$SLURM_ARRAY_TASK_ID]}"
+#this is easier todo with $(basename <file> suffix)
+sample3=$(basename ${args[$SLURM_ARRAY_TASK_ID]})
+sample2=$(basename ${sample3})
+sample="${sample2%.*}"
+echo "Starting with ${sample}"
+echo "Folder is ${args[0]}"
+
+
+
+out="."
+bbmap.sh -Xmx20g unpigz=t threads=2 minid=0.90 \
+ref=phyloFlash/LIB.all.final.phyloFlash.dbhits.fa nodisk \
+statsfile=$out/16SrRNA-statsfiles/${sample}.statsfile \
+scafstats=$out/16SrRNA-scafstats-statsfiles/${sample}.scafstats \
+covstats=$out/16SrRNA-cov-statsfiles/${sample}.covstat \
+rpkm=$out/16SrRNA-rpkm-statsfiles/${sample}.rpkm \
+sortscafs=f nzo=f ambiguous=all local=t \
+in="${args[0]}/${sample}/filter/${sample}_r1_nohost.fastq.gz" \
+in2="${args[0]}/${sample}/filter/${sample}_r2_nohost.fastq.gz" 
+
---
+++ scripts/python/make-splitmapping-stats.py
@@ -0,0 +1,47 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+
+def getBinName(f):
+    return Path(f).stem
+
+def main(args):
+    print("args" + str(args))
+    path =  "/work_beegfs/sukem127/clean/16ss/scafstats-statsfiles-unpaired-all/" #args[0]
+    # Read files into a list of pandas  frames
+    onlyfiles = list(map(lambda x: path + "/" + x, [f for f in listdir(path) if isfile(join(path, f))]))
+    print(onlyfiles)
+    dfs = list()
+    # open files and read them into a df
+    for f in onlyfiles:
+        df = pd.read_csv(f, sep='\t')
+        df = df[["#name", "unambiguousReads"]]
+        df["#name"] = df["#name"].str.split(pat=" ", expand=True)[0]
+        binName = getBinName(f)
+        df = df.rename(columns={"#name" : "#name", "unambiguousReads" : binName})
+
+        # Change name on the second colum to the binname so we can identify to which bin the amount of reads belongs
+        # add df to df list(wasteful but whatever)
+        dfs.append(df)
+
+    df = dfs[0]
+    for i in range(1, len(dfs) ):
+        print("merging "   + str(i))
+        df1 = dfs[i]
+
+        df = df.merge(df1, on="#name")
+
+    #print(dfs)
+    df.to_csv("allstats-unambiguousReads.csv", sep = "\t", index=False)
+
+
+if __name__ == '__main__':
+    print("WD")
+    args = sys.argv[1:]
+
+    main(args)
+    exit(0)
